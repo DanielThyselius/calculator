5c15c2e9e9b7eef6cc098360d0acb6826dc5340b
AntonFridlund <anton.fridlund@mail.com>
2021-12-17 19:47:26 +0100

    Add UI tests for Temperature menu option visibility on application startup

--- src/CalculatorUITests/NavigationMenuTests.cs
+++ src/CalculatorUITests/NavigationMenuTests.cs
@@ -155,6 +155,17 @@ namespace CalculatorUITests
             Assert.ThrowsException<WebDriverException>(() => CalculatorApp.Window.FindElementByAccessibilityId("Weight"));
         }
 
+        /// <summary>
+        /// Test that the Temperature navigation menu option is not visible on application startup.
+        /// </summary>
+        [TestMethod]
+        [Priority(0)]
+        public void Temperature_Menu_Option_Not_Visible_On_Startup()
+        {
+            // Assert that the Temperature navigation menu option is not visible.
+            Assert.ThrowsException<WebDriverException>(() => CalculatorApp.Window.FindElementByAccessibilityId("Temperature"));
+        }
+
         #endregion
     }
 }
