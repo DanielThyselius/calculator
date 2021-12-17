5b4c58d2051b920802fa924ab35558c9b5b48415
AntonFridlund <anton.fridlund@mail.com>
2021-12-17 19:42:21 +0100

    Add UI tests for Date menu option visibility on application startup

--- src/CalculatorUITests/NavigationMenuTests.cs
+++ src/CalculatorUITests/NavigationMenuTests.cs
@@ -100,6 +100,17 @@ namespace CalculatorUITests
             Assert.ThrowsException<WebDriverException>(() => CalculatorApp.Window.FindElementByAccessibilityId("Programmer"));
         }
 
+        /// <summary>
+        /// Test that the Date navigation menu option is not visible on application startup.
+        /// </summary>
+        [TestMethod]
+        [Priority(0)]
+        public void Date_Menu_Option_Not_Visible_On_Startup()
+        {
+            // Assert that the Date navigation menu option is not visible.
+            Assert.ThrowsException<WebDriverException>(() => CalculatorApp.Window.FindElementByAccessibilityId("Date"));
+        }
+
         #endregion
     }
 }
